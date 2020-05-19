2a81e74594bbbddef7dc9b458ed1f49a8675dae9
Bartlomiej2001 <57993203+Bartlomiej2001@users.noreply.github.com>
2020-05-19 11:29:33 +0200

    Add files via upload

---
+++ basinski.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 19 Maj 2020, 11:21
+-- Wersja serwera: 10.4.11-MariaDB
+-- Wersja PHP: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `basinski`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `marki`
+--
+
+CREATE TABLE `marki` (
+  `marka` text NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `marki`
+--
+
+INSERT INTO `marki` (`marka`, `id`) VALUES
+('Ferrari', 1),
+('BMW', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `samochody`
+--
+
+CREATE TABLE `samochody` (
+  `model` text NOT NULL,
+  `predkosc_maksymalna` text NOT NULL,
+  `0-100 km/h` text NOT NULL,
+  `idmarki` int(11) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `samochody`
+--
+
+INSERT INTO `samochody` (`model`, `predkosc_maksymalna`, `0-100 km/h`, `idmarki`, `id`) VALUES
+('458 Italia', '340 km/h', '3.5 s', 1, 1),
+('Aventador', '350 km/h', '2,9 s', 1, 2),
+('i8', '250 km/h', '4,4 s', 2, 3);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `marki`
+--
+ALTER TABLE `marki`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `samochody`
+--
+ALTER TABLE `samochody`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `marki`
+--
+ALTER TABLE `marki`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT dla tabeli `samochody`
+--
+ALTER TABLE `samochody`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
