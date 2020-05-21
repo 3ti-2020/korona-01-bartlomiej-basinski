ee3b78ba9488b7bbd07a8031b197aa7fb5fc1573
Bartlomiej2001 <57993203+Bartlomiej2001@users.noreply.github.com>
2020-05-21 20:29:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,21 +10,32 @@
 <body class="body">
     
 <div class="container">
+    <div class="buttons">
+        <span class="Wybierz">Wybierz motyw:</span>
+        <input type="submit" class="button_j" value="Jasny">
+        <input type="submit" class="button_c" value="ciemny">
+    </div>
     <h1 class="header">Bartłomiej Basiński</h1>
     <div class="sidebar">
         <p class="galeria">Galeria zdięć:</p><br>
         <ul>
-        <li><a class="linki" href="zdięcia_samochody.html">zdięcia samochodów</a></li>
-        <li><a class="linki" href="zdięcia_gór.html">Zdięcia Gór</a></li>
-        <li><a class="linki" href="zdięcia_miast.html">Zdięcia Miast</a></li>
+        <li><a class="linki" href="zdjęcia_samochody.php">zdjęcia samochodów</a></li>
+        <li><a class="linki" href="zdjęcia_gór.php">zdjęcia Gór</a></li>
+        <li><a class="linki" href="zdjęcia_miast.php">zdjęcia Miast</a></li>
     </ul>
     </div>  
     <div class="main">
-        <img src="Aventador.jpg" width="570px" height="300px">
-        <img src="rysy.jpg" width="570px" height="300px">
-        <img src="Nowy.jpg" width="570px" height="300px">
+        
+       <a href="zdjęcia_samochody.php"> <img src="Aventador.jpg" width="110%" height="300px"></a>
+       <a href="zdjęcia_gór.php"> <img src="rysy.jpg" width="100%" height="300px"></a>
+       <a href="zdjęcia_miast.php"> <img src="Nowy.jpg" width="100%" height="300px"></a>
     </div>
     <div class="footer"></div>
+
+</div>
+<script src="script.js"></script>
+</body>
+</html>
 
 </div>
 
